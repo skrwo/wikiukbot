881674ce4f6074c815f476bb80a54f5f36d41598
skrwo <165277183+skrwo@users.noreply.github.com>
2024-07-30 15:03:00 +0300

    implement a vercel serverless function? (idk if it works yet)

---
+++ src/api/webhook.mjs
@@ -0,0 +1,28 @@
+/**
+ * This module is vercel serverless function.
+ * See: 
+ * - https://grammy.dev/hosting/vercel;
+ * - https://vercel.com/docs/functions/quickstart;
+ */
+import { env } from "node:process"
+import { webhookCallback } from "grammy"
+import { bot } from "../bot.mjs"
+
+export const config = { runtime: "nodejs" }
+
+const callback = webhookCallback(bot, "std/http", {
+    secretToken: env.WEBHOOK_SECRET_TOKEN
+})
+/**
+ * 
+ * @param {Request} req 
+ */
+export async function POST(req) {
+    try {
+        return await callback(req)
+    } catch (e) {
+        // do not send 5xx responses
+        console.error("Failed to process webhook request:\n", e)
+        return new Response("Failed to process webhook request", { status: 200 })
+    }
+}
